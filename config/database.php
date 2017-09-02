4a7a6dc2baf4e2ffe1ff63e051f6142e94c0bd03
Mike Bywater <mike.bywater@enta.net>
2017-09-03 00:55:15 +0100

    Add testing db

--- config/database.php
+++ config/database.php
@@ -77,6 +77,11 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
         ],
+        'sqlite_testing' => [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ],
 
     ],
 
