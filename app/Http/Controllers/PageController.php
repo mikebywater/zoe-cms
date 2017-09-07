5834d5642792632c51567012a44d315abfbbf994
Mike Bywater <mike.bywater@enta.net>
2017-09-07 02:57:26 +0100

    Work on page controller

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    /**
+     * Actually render the page to the public
+     * @param $name
+     */
+    public function show($name)
+    {
+
+    }
+
+    /**
+     * Edit form for a page where you can set the field values
+     * @param $id
+     */
+    public function edit($id)
+    {
+
+    }
+
+    /**
+     * Create a new page
+     * @param Request $resuest
+     */
+    public function store(Request $resuest)
+    {
+
+    }
+
+
+}
