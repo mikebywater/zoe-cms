a297e42e40c87f4768de2c3352ef3dbdc35e215f
Mike Bywater <mike.j.bywater@gmail.com>
2018-06-30 11:27:32 +0100

    Sorted Reviews and added gallery back in

--- app/Services/ReviewService.php
+++ app/Services/ReviewService.php
@@ -11,7 +11,7 @@ class ReviewService
         $client = new Client();
         $json = $client->request('GET', 'https://maps.googleapis.com/maps/api/place/details/json', ['query' =>
             ['placeid' => 'ChIJmRGFnpR_cEgRteOCjbcPGFw' , 'key' => env('GPLACES_KEY')]]);
-        return collect(json_decode($json->getBody()->getContents())->result->reviews);
+        return collect(json_decode($json->getBody()->getContents())->result->reviews)->sortByDesc('time');
     }
 
     public function link()
